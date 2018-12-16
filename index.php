6276888df856edabbc1708011b5305db1d6eca30
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-16 20:24:55 +0100

    incrementCode

--- index.php
+++ index.php
@@ -1,23 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
- 
-</head>
+<?php 
+       include("./view/head.php");     
+?>
+
 <body>
-    
     <?php 
-       include("header.php");
+       include("./view/header.php");
        
     ?>
     
     <ul>
-        <li><a href="view/logIn.php">Click here</a></li>
-        <li><a href="newTask/fetchData.php">Get all ifo from json </a></li>
+        <li><a href="./view/logIn.php">Click here</a></li>
+        <li><a href="./jsonOperation/fetchData.php">Get all ifo from json </a></li>
 
     </ul>
     
