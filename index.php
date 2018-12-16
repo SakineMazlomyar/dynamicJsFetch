aa10e485950ae69b4c80f7112c4d4e19d669e6c8
SakineMazlomyar <mazlomyar12@gmail.com>
2018-12-16 20:15:32 +0100

    modifiedIndex

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+ 
+</head>
+<body>
+    
+    <?php 
+       include("header.php");
+       
+    ?>
+    
+    <ul>
+        <li><a href="view/logIn.php">Click here</a></li>
+        <li><a href="newTask/fetchData.php">Get all ifo from json </a></li>
+
+    </ul>
+    
+</body>
+</html>
